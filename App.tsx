e87e36815cad8d39595068b57d3f5fcd02b168b0
Tareq <tareq@threls.com>
2024-04-03 14:20:47 +0600

    updated the sample

--- App.tsx
+++ App.tsx
@@ -5,10 +5,15 @@
  * @format
  */
 
-import {canAddCardToWallet} from '@weavr-io/push-provisioning-react-native';
+import {
+  AddToWalletButton,
+  canAddCardToWallet,
+} from '@weavr-io/push-provisioning-react-native';
 import React, {useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Alert,
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -68,7 +73,8 @@ function App(): JSX.Element {
       console.log(res);
     });
   }, []);
-
+  const asset = require('./assets/Add-to-Google-Pay-Button-dark-no-shadow.png');
+  const assetSource = Image.resolveAssetSource(asset);
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -79,6 +85,28 @@ function App(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
+        <AddToWalletButton
+          androidAssetSource={assetSource}
+          iOSButtonStyle="onLightBackground"
+          style={styles.payButton}
+          cardHolderName={'Gordon Farrugia'}
+          cardDescription="Description"
+          cardLastFour={'6571'}
+          cardBrand={'mastercard'}
+          cardId={'111261918672650256'}
+          authenticationToken={
+            'eyJraWQiOiJnZW5lcmF0b3IiLCJhbGciOiJFUzI1NiJ9.eyJTWVNURU0iOiJmYWxzZSIsInN1YiI6IlJPT1QsMTA0MTg4Mjg1NzI1Mzc2NTIxIiwiVE9LRU5fUFJPVklERVIiOiJFTUFJTF9BTkRfUEFTU1dPUkQiLCJSQU5ET00iOiItMjM5MjI4MjQxNDgzMTQ2MDEzNCIsIklERU5USVRZX0lEIjoiMTA0MTg4Mjg1NzI1Mzc2NTIxIiwiSURFTlRJVFlfVFlQRSI6ImNvbnN1bWVycyIsIlBFUlBFVFVBTCI6ImZhbHNlIiwiVE9LRU5fVFlQRSI6IkFDQ0VTUyIsIlRFTkFOVF9JRCI6IjIiLCJJTVBFUlNPTkFUT1JfU0VTU0lPTl9JRCI6IjAiLCJTRVNTSU9OX0lEIjoiMTExMjYxOTA0NzkzMzA1MTA0IiwiUFJPR1JBTU1FX0lEIjoiMTAzMDc5MDg4NjU2MjIwMTY4IiwiREVWSUNFX0lEIjoiIiwiSU1QRVJTT05BVEVEIjoiZmFsc2UiLCJBVVRIX0dST1VQX0lEIjoiIn0.KkbylGKxLUIjWuP05s47kyPxLB1pNiLoE5_gdMoQqtRbz_wKOF-RKRezXFs3EQSPJA2wD4NGmkaQELcXfckyWA'
+          }
+          // debug
+          onComplete={({error}) => {
+            Alert.alert(
+              error ? error.code : 'Success',
+              error
+                ? error.message
+                : 'Card was successfully added to the wallet.',
+            );
+          }}
+        />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
@@ -119,6 +147,12 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  payButton: {
+    width: '65%',
+    height: 50,
+    marginTop: 60,
+    alignSelf: 'center',
   },
 });
 
