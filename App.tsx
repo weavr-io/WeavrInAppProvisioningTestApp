7831f67fc77e6476338997f27ac122b029288f4f
Tareq <tareq@threls.com>
2024-05-23 14:35:27 +0600

    updated SDK

--- App.tsx
+++ App.tsx
@@ -7,10 +7,12 @@
 
 import {
   AddToWalletButton,
+  GooglePayCheckResult,
   canAddCardToWallet,
+  checkGooglePayAvailability,
+  initWPP,
 } from '@weavr-io/push-provisioning-react-native';
 import React, {useEffect} from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   Alert,
   Image,
@@ -18,48 +20,10 @@ import {
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -68,6 +32,8 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
   useEffect(() => {
+    initWPP(token);
+
     // Perform a card status check to see if card can be added
     canAddCardToWallet('6751', 'mastercard').then(res => {
       console.log(res);
@@ -75,6 +41,8 @@ function App(): JSX.Element {
   }, []);
   const asset = require('./assets/Add-to-Google-Pay-Button-dark-no-shadow.png');
   const assetSource = Image.resolveAssetSource(asset);
+  const token =
+    'eyJraWQiOiJnZW5lcmF0b3IiLCJhbGciOiJFUzI1NiJ9.eyJTWVNURU0iOiJmYWxzZSIsInN1YiI6IlJPT1QsMTA0MTg4Mjg1NzI1Mzc2NTIxIiwiVE9LRU5fUFJPVklERVIiOiJFTUFJTF9BTkRfUEFTU1dPUkQiLCJSQU5ET00iOiItMjM5MjI4MjQxNDgzMTQ2MDEzNCIsIklERU5USVRZX0lEIjoiMTA0MTg4Mjg1NzI1Mzc2NTIxIiwiSURFTlRJVFlfVFlQRSI6ImNvbnN1bWVycyIsIlBFUlBFVFVBTCI6ImZhbHNlIiwiVE9LRU5fVFlQRSI6IkFDQ0VTUyIsIlRFTkFOVF9JRCI6IjIiLCJJTVBFUlNPTkFUT1JfU0VTU0lPTl9JRCI6IjAiLCJTRVNTSU9OX0lEIjoiMTExMjYxOTA0NzkzMzA1MTA0IiwiUFJPR1JBTU1FX0lEIjoiMTAzMDc5MDg4NjU2MjIwMTY4IiwiREVWSUNFX0lEIjoiIiwiSU1QRVJTT05BVEVEIjoiZmFsc2UiLCJBVVRIX0dST1VQX0lEIjoiIn0.KkbylGKxLUIjWuP05s47kyPxLB1pNiLoE5_gdMoQqtRbz_wKOF-RKRezXFs3EQSPJA2wD4NGmkaQELcXfckyWA';
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -94,9 +62,7 @@ function App(): JSX.Element {
           cardLastFour={'6571'}
           cardBrand={'mastercard'}
           cardId={'111261918672650256'}
-          authenticationToken={
-            'eyJraWQiOiJnZW5lcmF0b3IiLCJhbGciOiJFUzI1NiJ9.eyJTWVNURU0iOiJmYWxzZSIsInN1YiI6IlJPT1QsMTA0MTg4Mjg1NzI1Mzc2NTIxIiwiVE9LRU5fUFJPVklERVIiOiJFTUFJTF9BTkRfUEFTU1dPUkQiLCJSQU5ET00iOiItMjM5MjI4MjQxNDgzMTQ2MDEzNCIsIklERU5USVRZX0lEIjoiMTA0MTg4Mjg1NzI1Mzc2NTIxIiwiSURFTlRJVFlfVFlQRSI6ImNvbnN1bWVycyIsIlBFUlBFVFVBTCI6ImZhbHNlIiwiVE9LRU5fVFlQRSI6IkFDQ0VTUyIsIlRFTkFOVF9JRCI6IjIiLCJJTVBFUlNPTkFUT1JfU0VTU0lPTl9JRCI6IjAiLCJTRVNTSU9OX0lEIjoiMTExMjYxOTA0NzkzMzA1MTA0IiwiUFJPR1JBTU1FX0lEIjoiMTAzMDc5MDg4NjU2MjIwMTY4IiwiREVWSUNFX0lEIjoiIiwiSU1QRVJTT05BVEVEIjoiZmFsc2UiLCJBVVRIX0dST1VQX0lEIjoiIn0.KkbylGKxLUIjWuP05s47kyPxLB1pNiLoE5_gdMoQqtRbz_wKOF-RKRezXFs3EQSPJA2wD4NGmkaQELcXfckyWA'
-          }
+          authenticationToken={token}
           // debug
           onComplete={({error}) => {
             Alert.alert(
@@ -107,25 +73,6 @@ function App(): JSX.Element {
             );
           }}
         />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
       </ScrollView>
     </SafeAreaView>
   );
