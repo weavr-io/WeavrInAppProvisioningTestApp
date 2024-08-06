2ecf41aa433c0a0c6b6b6bbec2ad96ec05630e54
Tareq <tareq@threls.com>
2024-08-06 12:04:08 +0600

    updated the sample app

---
+++ ios/ExampleApp.swift
@@ -0,0 +1,16 @@
+//
+//  TestApp.swift
+//  WeavrInAppProvisioningTestApp
+//
+//  Created by Tareq Islam on 6/8/24.
+//
+
+import Foundation
+import weavr_react_native
+class TestApp{
+  func main(){
+    AddToWalletUtil.addPaymentPassWith(authenticationToken: <#T##String#>, clientPaymentCardId: <#T##String#>, certificates: <#T##[Data]#>, nonce: <#T##Data#>, nonceSignature: <#T##Data#>, completionHandler: <#T##(PKAddPaymentPassRequest) -> Void#>)
+  }
+  
+  
+}
